b00a71a7fbc004a5224754339e4e35bb32aa765e
chloesli <40907810+chloesli@users.noreply.github.com>
2020-05-26 12:16:29 +1000

    Initial commit

---
+++ socialapp-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyB4028lOimaMlDkF1cwvlRmKN8F0FUN3fk",
+    authDomain: "socialapp-5cb31.firebaseapp.com",
+    databaseURL: "https://socialapp-5cb31.firebaseio.com",
+    projectId: "socialapp-5cb31",
+    storageBucket: "socialapp-5cb31.appspot.com",
+    messagingSenderId: "131777563522",
+    appId: "1:131777563522:web:1b4a4f69184c95dd585d9b"
+};
