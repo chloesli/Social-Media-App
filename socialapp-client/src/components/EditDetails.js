24f4abb3fecabdc4f559847bf2ab52ebe72126ce
chloesli <40907810+chloesli@users.noreply.github.com>
2020-05-28 09:07:31 +1000

    Update EditDetails.js

--- socialapp-client/src/components/EditDetails.js
+++ socialapp-client/src/components/EditDetails.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles';
 
 // Redux
 import {connect} from 'react-redux'
-import { editUserDetials } from '../redux/actions/userActions'
+import { editUserDetails } from '../redux/actions/userActions'
 // MUI
 import Tooltip from '@material-ui/core/Tooltip';
+import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 import Button from '@material-ui/core/Button';
@@ -27,25 +28,93 @@ export class EditDetails extends Component {
         location: '',
         optne: false
     }
-
+    handleOpen = () => {
+        this.setState({open: true})
+        this.mapUserDetailsToState(this.props.credentials);
+    }
+    handleClose = () => {
+        this.setState({open: false})
+    }
     componentDidMount() {
         const {credentials} = this.props;
+        this.mapUserDetailsToState(credentials);
+    }
+    mapUserDetailsToState = (credentials) => {
         this.setState({
             bio: credentials.bio ? credentials.bio : '',
             website: credentials.website ? credentials.website : '',
             location: credentials.location ? credentials.location : '',
         })
     }
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+    };
+    handleSubmit = () => {
 
+    }
     render() {
         const {classes} = this.props;
         return (
             <Fragment>
                 <Tooltip title="Edit details" placement="top">
-                    <IconButton onClick={this.handleOpen} className={classess.button}>
+                    <IconButton onClick={this.handleOpen} className={classes.button}>
                         <EditIcon color="primary"/>
                     </IconButton>
                 </Tooltip>
+            
+                <Dialog 
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    fullWidth
+                    maxWidth="sm">
+                    <DialogTitle>Edit your details</DialogTitle>
+                    <DialogContent>
+                        <form>
+                            <TextField
+                                name="bio"
+                                type="text"
+                                label="Bio"
+                                multiline
+                                rows="3"
+                                placeholder="A short bio about yourself"
+                                className={classes.TextField}
+                                value={this.state.bio}
+                                onChange={this.handleChange}
+                                fullWidth/>
+                            <TextField
+                                name="website"
+                                type="text"
+                                label="Website"
+                                rows="3"
+                                placeholder="Your personal/professional website"
+                                className={classes.TextField}
+                                value={this.state.website}
+                                onChange={this.handleChange}
+                                fullWidth/>
+                            <TextField
+                                name="location"
+                                type="text"
+                                label="Location"
+                                rows="3"
+                                placeholder="Where you live"
+                                className={classes.TextField}
+                                value={this.state.location}
+                                onChange={this.handleChange}
+                                fullWidth/>
+                        
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.handleSubmit} color="primary">
+                            Save
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Fragment>
         )
     }
@@ -60,4 +129,4 @@ const mapStateToProps = (state) => ({
     credentials: state.user.credentials
 })
 
-export default connect(mapStateToProps, {editUserDetails})(withStyles(styles)(EditDetails));
+export default connect(mapStateToProps, { editUserDetails })(withStyles(styles)(EditDetails));
